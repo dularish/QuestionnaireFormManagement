e98afb2342f3c0c6d7754db60e5baf501d118eae
DularishAmbigai <dularish.ka@ambigai.net>
2021-03-22 09:40:31 +0530

    Added user role by default to user on Registering

--- FormsWeb/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ FormsWeb/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using FormsWeb.Server.Models;
+using FormsWeb.Shared;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -91,7 +92,7 @@ namespace FormsWeb.Server.Areas.Identity.Pages.Account
 
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
+                    await _userManager.AddToRoleAsync(user, Roles.User.ToString());
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
                         return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
